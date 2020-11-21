4140acadec41f4b4a56aa04d5c52b5d4b4cb34d9
Alex Turovets <madlilu177@gmail.com>
2020-11-21 12:32:53 +0300

    code added

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        
+        int CustomerAccount = 100;
+        int PaymentValue = 1500;
+        int Bonus = 0;
+        if (PaymentValue > 1000){
+            Bonus = PaymentValue/100;
+        }
+        System.out.print("Total Bonus = ");System.out.println(Bonus);
+        System.out.print("Total Customer Account = "); System.out.println(CustomerAccount + PaymentValue + Bonus);
     }
 }
